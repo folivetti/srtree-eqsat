7b3f522dd222c8555906ae5091e2390329e0ae84
folivetti <folivetti@ufabc.edu.br>
2023-06-23 11:53:20 -0300

    memory fix

--- src/Data/SRTree/EqSat.hs
+++ src/Data/SRTree/EqSat.hs
@@ -228,7 +228,7 @@ rewriteTree :: (Analysis a l, Language l, Ord cost) => [Rewrite a l] -> Int -> I
 rewriteTree rules n coolOff c t = fst $ equalitySaturation' (BackoffScheduler n coolOff) t rules c
 
 rewriteAll, rewriteConst :: Fix SRTree -> Fix SRTree
-rewriteAll   = rewriteTree  (rewritesBasic <> constReduction <> constFusion <> rewritesFun) 2500 30 cost
+rewriteAll   = rewriteTree  (rewritesBasic <> constReduction <> constFusion <> rewritesFun) 2000 30 cost
 rewriteConst = rewriteTree constReduction 100 10 cost
 
 rewriteUntilNoChange :: [Fix SRTree -> Fix SRTree] -> Int -> Fix SRTree -> Fix SRTree
